7b3c815db2dd6f37760e7c25c7a20d5d57bf6af9
jordan-barkway-bluefruit <jordan.barkway@bluefruit.co.uk>
2019-06-03 10:35:44 +0100

    - Added a unit test framework with an example test foe future work

---
+++ MusicPlayer/MusicPlayerTests/MusicPlayerTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MusicPlayerTests
+{
+    public class MusicPlayerTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
