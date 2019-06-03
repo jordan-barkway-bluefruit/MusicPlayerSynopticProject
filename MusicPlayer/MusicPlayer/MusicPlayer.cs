0ee5db6a3e8f9111a392983cd422c465d872c6a5
jordan-barkway-bluefruit <jordan.barkway@bluefruit.co.uk>
2019-06-03 10:08:24 +0100

    - Renamed Form 1 to MusicPlayer - Added a graphics folder to store all the graphics for the project - Added a 'PlayButton' which currently plays a generic Windows .wav audio file

---
+++ MusicPlayer/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class MusicPlayer : Form
+    {
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButtonClicked(object sender, EventArgs e)
+        {
+            SoundPlayer songSelected = new SoundPlayer(@"c:\Windows\Media\chord.wav");
+            songSelected.Play();
+        }
+    }
+}
